87c2c921a92feec241df4cf3f230bfa37a4ef201
tr7zw <tr7zw@live.de>
2021-12-04 02:05:58 +0100

    Forge 1.18

--- NEAForge/src/main/java/dev/tr7zw/notenoughanimations/NEAnimationsModForge.java
+++ NEAForge/src/main/java/dev/tr7zw/notenoughanimations/NEAnimationsModForge.java
@@ -1,28 +1,29 @@
 package dev.tr7zw.notenoughanimations;
 
+import net.minecraftforge.client.ConfigGuiHandler.ConfigGuiFactory;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmlclient.ConfigGuiHandler.ConfigGuiFactory;
 
 @Mod("notenoughanimations")
 public class NEAnimationsModForge extends NEAnimationsLoader {
 
-	public NEAnimationsModForge() {
+    public NEAnimationsModForge() {
         try {
             Class clientClass = net.minecraft.client.Minecraft.class;
-        }catch(Throwable ex) {
+        } catch (Throwable ex) {
             LOGGER.warn("Not Enough Animations Mod installed on a Server. Going to sleep.");
             return;
         }
-	    onEnable();
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+        onEnable();
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
                 () -> new IExtensionPoint.DisplayTest(
                         () -> ModLoadingContext.get().getActiveContainer().getModInfo().getVersion().toString(),
                         (remote, isServer) -> true));
-		ModLoadingContext.get().registerExtensionPoint(ConfigGuiFactory.class, () -> new ConfigGuiFactory((mc, screen) -> {
-            return createConfigScreen(screen);
-        }));
-	}
-	
+        ModLoadingContext.get().registerExtensionPoint(ConfigGuiFactory.class,
+                () -> new ConfigGuiFactory((mc, screen) -> {
+                    return createConfigScreen(screen);
+                }));
+    }
+
 }
