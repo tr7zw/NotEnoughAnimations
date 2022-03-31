b5754160f964698faa28751b25003f64f63a6588
tr7zw <tr7zw@live.de>
2022-04-01 01:31:06 +0200

    Add more handlers for vanilla animations, fixing some unintended overlap

---
+++ NEAShared/src/main/java/dev/tr7zw/notenoughanimations/animations/vanilla/RiptideAnimation.java
@@ -0,0 +1,37 @@
+package dev.tr7zw.notenoughanimations.animations.vanilla;
+
+import dev.tr7zw.notenoughanimations.access.PlayerData;
+import dev.tr7zw.notenoughanimations.animations.BasicAnimation;
+import dev.tr7zw.notenoughanimations.animations.BodyPart;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.player.AbstractClientPlayer;
+
+public class RiptideAnimation extends BasicAnimation {
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isValid(AbstractClientPlayer entity, PlayerData data) {
+        return entity.isAutoSpinAttack();
+    }
+
+    @Override
+    public BodyPart[] getBodyParts(AbstractClientPlayer entity, PlayerData data) {
+        return BodyPart.values();
+    }
+
+    @Override
+    public int getPriority(AbstractClientPlayer entity, PlayerData data) {
+        return 3600;
+    }
+
+    @Override
+    public void apply(AbstractClientPlayer entity, PlayerData data, PlayerModel<AbstractClientPlayer> model,
+            BodyPart part, float delta, float tickCounter) {
+        // Do nothing
+    }
+
+}
