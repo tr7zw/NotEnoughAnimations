da01dea3bd39194bcaa9d29edc7baa7fb9c04628
tr7zw <tr7zw@live.de>
2025-12-10 20:07:28 +0100

    Add spear to the singlehanded animations

--- src/main/java/dev/tr7zw/notenoughanimations/animations/vanilla/VanillaSingleHandedAnimation.java
+++ src/main/java/dev/tr7zw/notenoughanimations/animations/vanilla/VanillaSingleHandedAnimation.java
@@ -29,7 +29,7 @@ public class VanillaSingleHandedAnimation extends BasicAnimation {
     private ArmPose leftArmPose;
     //? if >= 1.21.11 {
 
-    private final EnumSet<ArmPose> singleHandedAnimatios = EnumSet.of(ArmPose.SPYGLASS, ArmPose.THROW_TRIDENT);
+    private final EnumSet<ArmPose> singleHandedAnimatios = EnumSet.of(ArmPose.SPYGLASS, ArmPose.THROW_TRIDENT, ArmPose.SPEAR);
     //? } else if >= 1.17.0 {
     /*
     private final EnumSet<ArmPose> singleHandedAnimatios = EnumSet.of(ArmPose.SPYGLASS, ArmPose.THROW_SPEAR);
