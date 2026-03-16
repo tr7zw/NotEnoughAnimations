001e1e25b9a8a9c42c56517eb17fe82bf7e25b9d
Elena Winters <1693832+elenawinters@users.noreply.github.com>
2026-03-16 13:55:34 -0500

    feat: configurable relative head/body angle (#320)
    * feat: configurable relative head/body angle
    
    * fix: build
    
    also add a lil guard thing
    
    * cleanup: guard code
    
    this does not fix the build issue. waiting till tomorrow to figure that out
    
    * fix: guard code
    
    * cleanup: use Inject instead of mixinextras

--- src/main/java/dev/tr7zw/notenoughanimations/mixins/PlayerEntityMixin.java
+++ src/main/java/dev/tr7zw/notenoughanimations/mixins/PlayerEntityMixin.java
@@ -8,9 +8,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import dev.tr7zw.notenoughanimations.access.PlayerData;
 import dev.tr7zw.notenoughanimations.logic.PlayerTransformer;
+import dev.tr7zw.notenoughanimations.versionless.NEABaseMod;
 import dev.tr7zw.notenoughanimations.versionless.animations.DataHolder;
 import lombok.Getter;
 import lombok.Setter;
@@ -45,6 +47,22 @@ public class PlayerEntityMixin implements PlayerData {
         updateRenderLayerItems();
         setRotateBodyToHead(false);
     }
+
+    //? if >= 1.20.4 {
+    @Inject(method = "getMaxHeadRotationRelativeToBody", at = @At("HEAD"), cancellable = true)
+    protected void overrideMaxHeadRoationRelativeToBody(CallbackInfoReturnable<Float> ci) {
+        if (NEABaseMod.config.maxBlockingAngle < 0.0f || NEABaseMod.config.maxBlockingAngle > 15.0f) {
+            NEABaseMod.config.maxBlockingAngle = 15.0f;
+        }
+
+        if (NEABaseMod.config.maxNormalAngle < 0.0f || NEABaseMod.config.maxNormalAngle > 50.0f) {
+            NEABaseMod.config.maxNormalAngle = 50.0f;
+        }
+
+        Player player = (Player) (Object) this;
+        ci.setReturnValue(player.isBlocking() ? NEABaseMod.config.maxBlockingAngle : NEABaseMod.config.maxNormalAngle);
+    }
+    //? }
 
     @Override
     public int isUpdated(int frameId) {
